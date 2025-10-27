336fd4b0b1293f3839f0bc3906a95958c8142b48
alessandrofelder <alessandrofelder@users.noreply.github.com>
2025-10-27 14:39:10 +0000

    mock implementations for different algos

---
+++ toy_snake/ingest.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+def ingest_data_algo1(path: Path):
+    print("ingest_algo_1")
+
+def ingest_data_algo2(path: Path):
+    print("ingest_algo_2")
+
+    
---
+++ toy_snake/preprocess.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+def preprocess_algo1(path: Path):
+    print("preprocess_algo_1")
+
+def ingest_data_algo2(path: Path):
+    print("preprocess_algo_2")
